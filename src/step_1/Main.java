7f48a9daf141ea6d21830a1e3239c088b4653c02
fotistsiou <fotis.tsioumas@outlook.com>
2025-07-12 17:36:33 +0300

    Update Project

---
+++ src/step_1/Main.java
@@ -0,0 +1,18 @@
+package step_1;
+
+/**
+ * Simplest estimation
+ * -------------------
+ * Description
+ * Ever wondered how to assess if the text is hard or easy to read? For a human, this task is pretty simple: you just
+ * read the text and feel if you're struggling or not. But how to teach a computer do that? In this project, you will
+ * write such a program.
+ * Firstly, let's create a simple program. If a text contains more than 100 symbols (including spaces and punctuation),
+ * then it is considered hard to read. Otherwise, the text is considered easy to read. If a text contains exactly 100
+ * symbols, it is still easy to read.
+ * The input contains a single line of text. Output "HARD" if the text is hard to read and "EASY" if the text is easy to
+ * read.
+ */
+
+public class Main {
+}
---
+++ src/step_3/Main.java
@@ -0,0 +1,23 @@
+package step_3;
+
+/**
+ * Score!
+ * ------
+ * Description
+ * In this stage, you will program the Automated readability index (ARI). It was introduced in 1968 and a lot of
+ * research works rely on this.
+ * This article on ARI (https://en.wikipedia.org/wiki/Automated_readability_index) can give you more specifics on the
+ * application and age brackets.
+ * Also, your program should read a file instead of typing a text manually. You should pass the filename through the
+ * command line arguments.
+ * The program should output the score itself and an approximate age needed to comprehend the text.
+ * Use the appropriate rounding function to calculate the score as integer.
+ * You should also print how many characters, words, and sentences the text has.
+ * The number of characters is any visible symbol (so, in the real text it's everything except space, newline "\n" and
+ * tab "\t").
+ * Notice, that the text can contain multiple lines, not just a single line like in the previous stages. You should
+ * analyze all the lines.
+ */
+
+public class Main {
+}
